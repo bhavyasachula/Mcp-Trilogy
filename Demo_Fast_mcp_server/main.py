b7463b05e9e8fd9d2a17e25ec7381773f6beb06c
bhavya sachula <bhavyasachula07@gmail.com>
2026-05-08 23:25:38 +0530

    solved uv

---
+++ Demo_Fast_mcp_server/main.py
@@ -0,0 +1,18 @@
+from fastmcp import FastMCP
+import random
+
+"""Instance of MCP"""
+mcp = FastMCP(name="Demo Server")
+
+@mcp.tool
+def roll_dice(n_dice:int = 1):
+    """Roll the n_dice and return the result"""
+    return [random.randint(1,6) for _ in range(n_dice)]
+
+@mcp.tool
+def add_num(a:float,b:float):
+    """Adds the given number"""
+    return (a+b);
+
+if __name__ == "__main__":
+    mcp.run()
