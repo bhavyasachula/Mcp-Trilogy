c7fc1a58bd37caecb3d9c043d55d6ddfed6e9531
bhavya sachula <bhavyasachula07@gmail.com>
2026-05-09 12:35:13 +0530

    Added a Demo-server to claude desktop

--- Demo_Fast_mcp_server/main.py
+++ Demo_Fast_mcp_server/main.py
@@ -9,6 +9,7 @@ def roll_dice(n_dice:int = 1):
     """Roll the n_dice and return the result"""
     return [random.randint(1,6) for _ in range(n_dice)]
 
+
 @mcp.tool
 def add_num(a:float,b:float):
     """Adds the given number"""
@@ -16,3 +17,13 @@ def add_num(a:float,b:float):
 
 if __name__ == "__main__":
     mcp.run()
+
+"""
+    Default path of claude should be Appdata/Roaming/Claude
+    Otherwise i will give u an 
+    error:can find the main.py
+    first run :-
+    'uv run fastmcp install claude-desktop filename(In my case main.py)'
+    if this doesn't work use
+    uvx run fastmcp install filename(main.py)
+"""
